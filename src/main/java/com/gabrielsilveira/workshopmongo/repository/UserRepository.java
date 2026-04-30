31c924f8e33aaa2240593b82ef60a28342f208f0
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-04-30 12:56:08 -0300

    Connecting MongoDB with repository and service

---
+++ src/main/java/com/gabrielsilveira/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gabrielsilveira.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabrielsilveira.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+}
