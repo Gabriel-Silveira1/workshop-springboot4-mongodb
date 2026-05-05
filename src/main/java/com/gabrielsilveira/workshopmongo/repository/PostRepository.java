a8107322f532b5885ee623c52a33e16b162c1926
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-05-05 17:01:38 -0300

    Simple query with @Query

--- src/main/java/com/gabrielsilveira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gabrielsilveira/workshopmongo/repository/PostRepository.java
@@ -3,9 +3,12 @@ package com.gabrielsilveira.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.gabrielsilveira.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
